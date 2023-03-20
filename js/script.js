df944695b71617c4c5df6196a62f2009a17fe51b
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-03-20 16:11:57 +0100

    First 3 steps in JS

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@
  *      [] Prendo il valore del livello (bonus)
  *      [] In base al livello selezionato imposto il numero 
  *      delle celle (bonus)
- *      [] Trovo il container nella griglia della pagina
- *      [] Tolgo la scritta di benvenuto
- *      [] Tolgo l'hidden dalla grid
+ *      [X] Trovo il container nella griglia della pagina
+ *      [X] Tolgo la scritta di benvenuto
+ *      [X] Tolgo l'hidden dalla grid
  *      [] Genero la griglia con il ciclo for
  *      [] Per ognuno dei numeri (ciclo):
  *          [] Creare la cella
@@ -16,4 +16,13 @@
  * [] Al click su una cella:
  *      [] la cella cambia il colore
  *      [] stampare il numero della cella nella console
- * /
+ */
+
+document.getElementById("btn-play").addEventListener("click", startGame);
+
+function startGame() {
+    const grid = document.querySelector(".grid");
+    const introText = document.querySelector("h2");
+    introText.classList.add("hidden");
+    grid.classList.remove("hidden");
+}
