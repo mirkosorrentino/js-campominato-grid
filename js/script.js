5a9b9e89d23b54444ffc96e702b66154cf501d20
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-03-20 16:04:07 +0100

    Instruction in human language on JS

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/**
+ * [] Al click sul bottone:
+ *      [] Prendo il valore del livello (bonus)
+ *      [] In base al livello selezionato imposto il numero 
+ *      delle celle (bonus)
+ *      [] Trovo il container nella griglia della pagina
+ *      [] Tolgo la scritta di benvenuto
+ *      [] Tolgo l'hidden dalla grid
+ *      [] Genero la griglia con il ciclo for
+ *      [] Per ognuno dei numeri (ciclo):
+ *          [] Creare la cella
+ *          [] Inserire il numero nella cella
+ *          [] Aggiungere l'event listener alla cella
+ *          [] Aggiungere la cella creata nel container della griglia
+ * 
+ * [] Al click su una cella:
+ *      [] la cella cambia il colore
+ *      [] stampare il numero della cella nella console
+ * /
